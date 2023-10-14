9f1e82619841858c8438e385448ecb573f37aae6
ClaytonMalarkey <malarkey1985@gmail.com>
2023-10-13 19:21:17 -0600

    testing vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <template-fun>
+  </template-fun>
+</template>
+
+<script>
+import TemplateFun from './components/TemplateFun.vue'
+
+export default {
+    name: 'App',
+    components: {
+      'template-fun': TemplateFun
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TemplateFun.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="TemplateFun">
+        <h1>{{ blog_title }}</h1>
+        <p>{{ blog_content }}</p>
+        <img :src="require(`@/assets/${blog_img_ulr}`)"/>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+        data(){
+            return {
+                blog_content:"test content",
+                blog_title: "blog",
+                blog_img_ulr: "logo.png"
+            }
+        }
+
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  
+  </style>
