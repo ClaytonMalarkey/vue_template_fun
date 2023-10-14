bd4c024bf0036680479ac7a40c2b06f0d80325cd
ClaytonMalarkey <malarkey1985@gmail.com>
2023-10-13 19:49:02 -0600

    second commit

--- src/components/TemplateFun.vue
+++ src/components/TemplateFun.vue
@@ -1,9 +1,15 @@
 <template>
-    <div class="TemplateFun">
-        <h1>{{ blog_title }}</h1>
-        <p>{{ blog_content }}</p>
-        <img :src="require(`@/assets/${blog_img_ulr}`)"/>
+    <div>
+        <button @click=toggle_TF>CLICK ME</button>
+        <div v-if="is_true == true" class="TemplateFun">
+        
+            <h1>{{ blog_title }}</h1>
+            <p>{{ blog_content }}</p>
+            <img :src="require(`@/assets/${blog_img_ulr}`)" > 
+        </div>
     </div>
+    
+    
   </template>
   
   <script>
@@ -12,7 +18,14 @@
             return {
                 blog_content:"test content",
                 blog_title: "blog",
-                blog_img_ulr: "logo.png"
+                blog_img_ulr: "logo.png",
+                is_true : false
+            }
+        },
+        methods:{
+            toggle_TF(){
+                this.is_true = !this.is_true;
+                console.log(this.is_true);
             }
         }
 
